72defad74d470262a2efe53943d5d6f05c322505
pekoy <mhmmdfaiz160@gmail.com>
2024-02-04 13:49:56 +0700

    update: profile-pic

--- lib/settingpage/settingpage.dart
+++ lib/settingpage/settingpage.dart
@@ -11,9 +11,40 @@ class SettingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.amber,
-        title: Text("Forum"),
+        backgroundColor: Color(0xFFF6F6F6),
+        title: Text("Settings"),
         centerTitle: true,
+        leading: Icon(
+          Icons.qr_code_rounded,
+          color: Color(0xFF037EE5),
+        ),
+        actions: [
+          Padding(
+            padding: EdgeInsets.all(16), 
+            child: Text(
+              "Edit",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+              fontSize: 17,
+              fontWeight: FontWeight.w400,
+              color: Color(0xFF037EE5)
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 16,),
+            Center(
+              child: CircleAvatar(
+                radius: 70,
+                backgroundImage: AssetImage("image"),
+              )
+            )
+          ],
+        ),
       ),
     );
   }
